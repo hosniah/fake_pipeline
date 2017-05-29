606fca26237ef23ed6cf79695204cabf59a85e9c
Ahmed Hosni <ahhosni@VISTAPRINT.NET>
2017-05-29 12:30:18 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# fake_pipeline
+# simulate a minimal Jenkins pipeline that serves as a starting point for Blue Ocean plugin testing.
+
+# At time of testing, https://devvbujenkins.vistaprint.net/ is running Jenkins ver. 2.46.1.
+# Blue Ocean version is 1.0.1 
---
+++ src/test/java/test/DummyTests.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.Test;
+
+public class DummyTests {
+  protected void run() {
+    System.out.print("Hello Blue Ocean");
+  }
+
+  @Test public void test1() {
+    System.out.print("Hello VIPS Support");
+  }
+
+  @Test public void test2() {
+    run();
+  }
+
+  @Test public void test3() {
+    run();
+  }
+
+  @Test public void test4() {
+    run();
+  }
+
+  @Test public void test5() {
+    run();
+  }
+
+  @Test public void test6() {
+    run();
+  }
+
+}
